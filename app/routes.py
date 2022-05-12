3d3de38420624f1b8efdb251f767c65e5bc324bb
MuhammadMaazAhmed <maazahmedpoke@gmail.com>
2022-05-13 00:33:59 +0800

    basic backend template done

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template, url_for
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "helsdlo"
+    return render_template('base.html')
