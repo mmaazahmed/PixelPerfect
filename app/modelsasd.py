7ca5d8f55b729cc89cb24504720561547377d95a
MuhammadMaazAhmed <maazahmedpoke@gmail.com>
2022-05-22 20:20:38 +0800

    fixed conflicts

--- app/modelsasd.py
+++ app/modelsasd.py
@@ -3,26 +3,24 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 #bcrypt generate_password_hash(), check_generate_password_hash()
- @login_manager.user_loader
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
 class User(db.model,UserMixin):
-    __tablename__'users'
+    __tablename__='users'
 
     id = db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(64,unique=True,nullable=False))
-    email=db.Column(db.String(128), unique=True, nullable=False))
+    username=db.Column( db.String(64),unique=True,nullable=False)
+    email=db.Column(db.String(128), unique=True, nullable=False)
     password_hash=db.Column(db.String(128))
 
     def __init__(self, user_id,username, password, email):
         self.username = username
         self.password = generate_password_hash(password)
-<<<<<<< HEAD:app/modelsasd.py
-=======
+
         self.name = name
 
->>>>>>> eb7d238d7988a56c5cba1cc73ba357c8dd20a792:app/models.py
         self.email = email
     
     def __repr__(self):
@@ -35,17 +33,16 @@ class User(db.model,UserMixin):
 
 class Player_history(db.model):
     
-    __table_name= 'player_history'
+    __tablename__= 'player_history'
     id = db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(64,unique=True,nullable=False))
-    email=db.Column(db.String(128), unique=True, nullable=False))
+    username=db.Column(db.String(64),unique=True,nullable=False)
+    email=db.Column(db.String(128), unique=True, nullable=False)
     password_hash=db.Column(db.String(128))
 
 
     def __init__(self, ):
-        super(PlayerHisto_hy,db.model.
-    _
-    __table_name= 'player_history'_init__(*args))
+        super(PlayerHisto_hy,db.model)
+    __tablename__= 'player_history'
         
 
         
