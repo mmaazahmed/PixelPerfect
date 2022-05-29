d0b3bef0a406e15e5c4821598534b625e6f2f99b
Pablo <pablonaydenovau@gmail.com>
2022-05-29 21:51:36 +0800

    Basic stats imp, needs to be tested with DB

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,2 +1,2 @@
-from app.api import guessprocessing, correct_answer
+from app.api import guessprocessing, correct_answer, statstore, statCheck
 from app import app
---
+++ app/api/statstore.py
@@ -0,0 +1,31 @@
+from app import app, db
+from flask import jsonify, url_for, request
+from app.api.errors import bad_request, error_response
+from app.models import Player_History, Image_Table
+from datetime import date
+from flask_login import AnonymousUserMixin, current_user
+app.route('/api/storestats',methods=['POST'])
+def store():
+    today = date.today()
+    date = today.strftime("%m/%d/%Y")
+    data = request.json
+    Image_Table.query.filter_by(date=date)
+    username = current_user.username
+    ans = data['answer_history']
+    count = data['answer_count']
+    winstatus = data['win']
+    img_id = None
+    player = Player_History.query.filter_by(username=username).all()[-1]
+    if player.date_submitted == date:
+        return error_response(409, "You've already submitted a score today")
+    elif (current_user is AnonymousUserMixin):
+        return error_response(409, "Anonymous User, score won't be saved")
+    else:
+        submission = Player_History(username,ans,count,img_id,date)
+        db.session.add(submission)
+        db.session.commit()
+
+
+
+
+
