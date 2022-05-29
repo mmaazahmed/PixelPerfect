3f557f17a3a909924eec1bdefe81b94e69956db5
MuhammadMaazAhmed <maazahmedpoke@gmail.com>
2022-05-29 19:51:26 +0800

    set up the databse again with migrations

--- app/__init__.py
+++ app/__init__.py
@@ -4,19 +4,18 @@ from flask import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from PixelPerfect import PixelPerfect
-#from flask_sqlalchemy import SQlAlchemy
 
 
 app= Flask(__name__)
-PixelPerfect.initialiseGame()
+# PixelPerfect.initialiseGame()
 app.config.from_object(Config)  
 
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY']=SECRET_KEY
 
 basedir= os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir,'database.db'))
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///pixel-perfect.fb'
+SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir,'app.db')
+app.config['SQLALCHEMY_DATABASE_URI']= SQLALCHEMY_DATABASE_URI
 
 
 
@@ -24,5 +23,5 @@ db = SQLAlchemy(app)
 migrate=Migrate(app,db)
 
 
-from app import routes,api
+from app import routes,api,models
 
--- app/models.py
+++ app/models.py
@@ -1,27 +1,27 @@
-from app import db, login_manager
+from app import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-#bcrypt generate_password_hash(), check_generate_password_hash()
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
+# #bcrypt generate_password_hash(), check_generate_password_hash()
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(user_id)
 
-class User(db.model,UserMixin):
-    __tablename__='users'
+class User(UserMixin,db.Model):
+    # __tablename__='users'
 
     id = db.Column(db.Integer,primary_key=True)
     username=db.Column( db.String(64),unique=True,nullable=False)
     email=db.Column(db.String(128), unique=True, nullable=False)
     password_hash=db.Column(db.String(128))
 
-    def __init__(self, user_id,username, password, email):
-        self.username = username
-        self.password = generate_password_hash(password)
+    # def __init__(self, user_id,username, password, email):
+    #     self.username = username
+    #     self.password = generate_password_hash(password)
 
-        self.name = name
+    #     self.name = username
 
-        self.email = email
+    #     self.email = email
     
     def __repr__(self):
         return f'<User {self.username}>'
@@ -31,18 +31,28 @@ class User(db.model,UserMixin):
     
    
 
-class Player_history(db.model):
+class Images(db.Model):
+    # __tablename__= 'Images'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    answer = db.Column(db.String, nullable=False)
+    date = db.Column(db.String, nullable=False)
+    user_hist = db.relationship("Player_history")
+
+class Player_history(db.Model):
     
-    __tablename__= 'player_history'
-    id = db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(64),unique=True,nullable=False)
-    email=db.Column(db.String(128), unique=True, nullable=False)
-    password_hash=db.Column(db.String(128))
+    # __tablename__= 'player_history'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False) #use Username to return results for that username
+    answer_history = db.Column(db.String, nullable=False) #Have Large Sequence
+    answer_count = db.Column(db.Integer, nullable=False)
+    date_submitted = db.Column(db.String, nullable=False)
+    win = db.Column(db.Boolean, default=False, nullable=False)
+    img_id = db.Column(db.Integer, db.ForeignKey('images.id'))
+    
 
 
-    def __init__(self, ):
-        super(PlayerHistory,db.model)
-    __tablename__= 'player_history'
         
 
         
